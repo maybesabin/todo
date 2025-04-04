7048893089559c046cbf723df9242bfda8f835d3
unknown <sabin@epraivid.com>
2025-04-05 01:58:11 +0545

    cors error fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ connectToDb();
 const PORT = process.env.PORT || 3000;
 
 //middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
