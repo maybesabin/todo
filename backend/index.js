dc73396f1b59b7ec5759b0cd304855b11d60327c
unknown <sabin@epraivid.com>
2025-04-04 10:33:43 +0545

    cors error fix

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ const connectToDb = require("./config/db.config");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes")
 
-cors();
 dotenv.config()
 connectToDb();
 
 const PORT = process.env.PORT || 3000;
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
