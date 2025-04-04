6ab4ba3e23bbf7f72e11d946c3bd118a5a0ba174
unknown <sabin@epraivid.com>
2025-04-04 17:54:22 +0545

    mongodb error fix

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to mongodb ✅")
     } catch (error) {
         console.log(error)
